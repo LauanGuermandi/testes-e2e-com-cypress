7ee704ffe7630141e87b5d35e90fd9da0b58505f
Lauan Guermandi <lauanguermandi@gmail.com>
2023-02-05 02:00:42 -0300

    add signup and login tests

---
+++ cypress/support/commands.js
@@ -0,0 +1,7 @@
+Cypress.Commands.add('signUp', (emailAddress, password) => {
+  cy.visit('/signup')
+  cy.get('#email').type(emailAddress)
+  cy.get('#password').type(password, { log: false })
+  cy.get('#confirmPassword').type(password, { log: false })
+  cy.contains('button', 'Signup').click()
+})
